33cdf4a5d037f66f307f8a809d232ea70faab504
jorgielyn <irangabriellef14@gmail.com>
2019-11-29 10:17:48 +0800

    Hasol!!!!!!!!

--- index.js
+++ index.js
@@ -3,59 +3,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const port = 3000
-
-// Create express app
+const port = 3000;
+var Pusher = require('pusher')
 const app = express();
-// Parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-// Parse requests of content-type - application/json
-app.use(bodyParser.json())
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
-
-
-
-// Set up default mongoose connection
 let db_url = 'mongodb://127.0.0.1/iConnect';
 mongoose.connect(db_url, { useNewUrlParser: true });
-// Get the default connection
 var db = mongoose.connection;
-// Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-
-//
-// Let's start the exercise :
-//
-// During ALL the exercise the requests have to be connected with the database
-//
-// Context : We want to create a web application to manage a motorcyle Championship. 
-// ------------------------------------------------------------
-
-// Import Models
-// const user = require('./models/user.model');
 const Users = require('./models/user.model.js');
-// const PartneredUsers = require('./models/partneredUser.model.js')
 const Partner = require('./models/partnered.model.js');
 const Authorization = require('./models/authorization.model.js')
 const TrackingInfo = require('./models/trackingInfo.model.js')
 
-// Question 1 - Create a HTTP Request to add a users in the database :
-// When we create a user he doesn't have a score yet.
 // var x = [{ fname: "Aeromel", lname: "Laure", email: "aero@gmail.com", password: "1love@you", address: "Leyte", partneredId: "abc123" },
 // { fname: "Mibel", lname: "Paculanang", email: "mibel@gmail.com", password: "m1b3l@love", address: "Santander", partneredId: "mnb456" },
 // { fname: "Cherry", lname: "Herrera", email: "cherry@gmail.com", password: "c#3rryM@e", address: "Sultan-Kudarat", partneredId: "hjkl654" },
 // { fname: "Faye", lname: "Catalvas", email: "faye@gmail.com", password: "f@y3Erika", address: "Leyte", partneredId: "qwer45" },
 // { fname: "Mary", lname: "Tibs", email: "tibs@gmail.com", password: "t1b5M@ry", address: "Dalaguete", partneredId: "asdf12" }]
 app.post('/partners', (req, res) => {
-    let part = new Partner({ fname: "Mary", lname: "Tibs", email: "tibs@gmail.com", password: "t1b5M@ry", address: "Dalaguete", partneredId: "asdf12" });
+    let part = new Partner({fname: "Faye", lname: "Catalvas", email: "faye@gmail.com", password: "f@y3Erika", address: "Leyte", partneredId: "qwer45" });
 
     part.save((err, partner) => {
         if (err) {
@@ -127,22 +103,35 @@ app.post('/allPartners', (req, res) => {
     })
 })
 
-// app.post('/pusher', (req, res) => {
-//     var notifications = {
-//          "notifications": req.query
-//     }
-//     var pusher = new Pusher({
-//        appId: '906630',
-//        key: 'ea9fe3985cb69d3aff5d',
-//        secret: 'f4d20401c2e102900b46',
-//        cluster: 'ap1',
-//        encrypted: true
-//      });
-//         pusher.trigger('my-channel', 'my-event', notifications); 
-//         res.json({
-//          message: 'Successful'
-//      })
-//  })
+app.post('/onePartner/:part', (req, res) => {
+    Partner.find({email: req.params.part}, (err, partner) => {
+        if (err) {
+            res.status(404).send(err)
+        } else if (partner != null) {
+            res.json({ partner })
+        } else {
+            res.status(404).send("Error")
+        }
+    })
+})
+
+app.post('/pusher', (req, res) => {
+    var notify = {
+         notify: req.body
+    }
+    var pusher = new Pusher({
+       appId: '906630',
+       key: 'ea9fe3985cb69d3aff5d',
+       secret: 'f4d20401c2e102900b46',
+       cluster: 'ap1',
+       encrypted: true
+     });
+    pusher.trigger('form', 'auth', notify); 
+    res.json({
+        message: 'Successful'
+    })
+     console.log(notify)
+ })
 
  app.post('/authLetter', (req, res) => {
     let auth = new Authorization(req.body);
@@ -155,147 +144,6 @@ app.post('/allPartners', (req, res) => {
     });
 })
 
-// app.post('/tracking', (req, res) => {
-//     let track = new Tracking({
-//         trackingNo: req.body.track,
-//         form: req.body.subForm
-//     });
-
-//     track.save((err, track) => {
-//         if (err) {
-//             res.send(err)
-//         }
-//         res.json({ track })
-//     });
-// })
-
-// app.post('/addTrack', (req, res) => {
-//     let track = new Tracking({
-//         trackingNo: "09161571895",
-//         form: "The quick brown fox jumps over the lazy dog"
-//     });
-
-//     track.save((err, track) => {
-//         if (err) {
-//             res.send(err)
-//         }
-//         res.json({ track })
-
-//         let tracking = new TrackingInfo({
-//             trackingNo: Tracking._id,
-//             address: "America"
-//         });
-
-//         tracking.save((err, trackings) => {
-//             if(err) {
-//                 res.send(err)
-//             }
-//             res.json({ trackings })
-//         })
-//     });
-// })
-
-// app.post('/findTrack', (req, res) => {
-//     TrackingInfo.
-//         findOne({ trackingNo: '09161571895' }).
-//         populate('Tracking').
-//         exec(function (err, tracking) {
-//             if (err) return handleError(err);
-//             console.log('This is the output', tracking.trackingNo);
-//             res.json(trackingInfo.Tracking)
-//             // prints "The author is Ian Fleming"
-//         });
-// })
-
-
-// // Question 4 - Create a HTTP Request to update firstName or/and lastName of a user :
-// app.put('/update', (req, res) => {
-//     var id = req.body.id
-//     const updateFL = {
-//         firstName: "Mhine",
-//         lastName: "Laure"
-//     }
-
-//     user.findByIdAndUpdate(id,  updateFL, (err, updated) => {
-//         if(err){
-//             res.send(err)
-//         }
-//         res.json({updated})
-//     })
-// })
-
-
-// // Question 5 - Create a HTTP Request to ADD score of a user :
-// app.put('/addScore', (req, res) => {
-//     var id = req.body.id
-//     const addScore = {
-//         score: req.body.score
-//     }
-
-//     user.findByIdAndUpdate(id,  addScore, (err, updated) => {
-//         if(err){
-//             res.send(err)
-//         }
-//         res.json({updated})
-//     })
-// })
-
-
-// // Question 6 - Create a HTTP Request to delete one user :
-// app.delete('/delete', (req, res) => {
-//     user.findByIdAndDelete(req.body.id, (err, deleted) => {
-//         if(err){
-//             res.send(err)
-//         }
-//         res.json({deleted})
-//     })
-// })
-
-
-// // Question 7 - Create a HTTP Request to create motorcycles :
-// // For create a motorcycle you will need to create the model first..
-// app.post('/addMotorcycle', (req, res) => {
-//     let addMotor = new Motorcycle(
-//         {
-//             manufacturer: req.body.manu,
-//             displacement: req.body.dis,
-//             weight: req.body.weight
-//         }
-//     );
-
-//     addMotor.save((err, motor) => {
-//         if(err){
-//             res.send(err)
-//         }
-//         res.json(motor)
-//     });
-// })
-
-
-// // Question 8 - Create a HTTP Request to fetch all the motorcycles:
-// app.get('/allMotorcycles', (req, res) => {
-//     Motorcycle.find({}, (err, motor) => {
-//         if(err){
-//             res.send(err)
-//         }
-//         res.json({motor: motor})
-//     })
-// })
-
-
-// // Question 9 - Create a HTTP Request to fetch all the motorcycles associate to one user:
-// app.get('/allMotorcyclesuser', (req, res) => {
-//     user.find().where('Laure').all({Motorcycle})
-// })
-
-
-// BONUS 10 - Create a HTTP Request to to get the users ranking
-
-
-//
-// End of the exercise
-// ------------------------------------------------------------
-// listen for requests
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
